aa84432bf94f287c33a9f3631cabd8ae0c3ddcac
Mohamed Salman <mohamed.akj@gmail.com>
2024-04-09 12:07:25 +0530

    Added html response for home route to test azure web app service

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from . import models as models
 from .crud.routes import router
@@ -9,6 +10,19 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return """
+        <html>
+            <head>
+                <title>Some HTML in here</title>
+            </head>
+            <body>
+                <h1>Look ma! HTML!</h1>
+            </body>
+        </html>
+        """
 
 @app.get("/api/v1/healthcheck")
 async def main_route() -> dict:
